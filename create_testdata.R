cc743262dd14d657f2004cf8c324a2c62b593289
Susie Jentoft <coo@ssb.no>
2023-06-02 15:21:50 +0200

    Added avocado and cucumber examples in testdata

--- create_testdata.R
+++ create_testdata.R
@@ -7,7 +7,10 @@ testdata <- rbind(
     c('Jordbærsyltetøy hjemmelaget 400G',
         'Jordbær 60%, sukker, vann, fortykningsmiddel (pektin), surhetsregulerende middel (eplesyre), konserveringsmiddel (kaliumsorbat)',
         NA, "01182","011821","01183"),
-    c("Maiskorn til popcorn 400g", "Tørkede maiskorn", NA, '01173', '011732','01179')
+    c("Maiskorn til popcorn 400g", "Tørkede maiskorn", NA, '01173', '011732','01179'),
+    c("Stor agurk", NA, NA, "01171", "011714", "01172"),
+    c("Avokado spisemoden", NA, NA, "01171", "011719", "01161"),
+    c("Avokado spisemoden", NA, NA, "01161", "011719", "01161")
 )
 
 colnames(testdata) <- c('product_name', "ingreds", "temp", 'ecoicop5', 'ecoicop6', 'coicopUN')

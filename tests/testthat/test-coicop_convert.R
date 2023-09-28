ac8896c66cfe49de0d61cf2687a28014c4048434
Susie Jentoft <coo@ssb.no>
2023-09-28 14:31:23 +0200

    Added new testdata and tests for checking missing values and missing temperature variable

--- tests/testthat/test-coicop_convert.R
+++ tests/testthat/test-coicop_convert.R
@@ -1,4 +1,4 @@
-
+## Unittests for coicop converter
 
 test_that("Avocado is coded correctly", {
   ny <- coicop_convert(testdata,
@@ -11,4 +11,35 @@ test_that("Avocado is coded correctly", {
 })
 
 
+test_that("Code will run if temperature is missing",{
+  ny <- coicop_convert(testdata,
+                       ecoicop = "ecoicop5",
+                       varenavn = "product_name",
+                       coicop6 = "ecoicop6",
+                       ingredients = "ingreds")
+  expect_equal(ny[8], "01161")
+})
 
+
+test_that("Maxtemp is correctly converted to numeric",{
+  ny <- coicop_convert(testdata,
+                       ecoicop = "ecoicop5",
+                       varenavn = "product_name",
+                       coicop6 = "ecoicop6",
+                       ingredients = "ingreds",
+                       maxtemp = "temp")
+  expect_equal(ny[8], "01161")
+})
+
+
+test_that("Item missing name returns NA",{
+  expect_message(
+    ny <- coicop_convert(testdata,
+                         ecoicop = "ecoicop5",
+                         varenavn = "product_name",
+                         coicop6 = "ecoicop6",
+                         ingredients = "ingreds",
+                         maxtemp = "temp")
+  )
+  expect_true(is.na(ny[10]))
+})
